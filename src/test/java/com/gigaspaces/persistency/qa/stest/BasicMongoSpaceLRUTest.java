befab3f70e4fd5faa2f0c9b29d2488dc7de39a59
genzel <boris@gigaspaces.com>
2014-03-27 10:03:58 +0200

    GS-11263 added assert for like and rlike query read from space

--- src/test/java/com/gigaspaces/persistency/qa/stest/BasicMongoSpaceLRUTest.java
+++ src/test/java/com/gigaspaces/persistency/qa/stest/BasicMongoSpaceLRUTest.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
+import com.j_spaces.core.client.SQLQuery;
 import org.junit.Assert;
 
 
@@ -52,7 +53,18 @@ public class BasicMongoSpaceLRUTest extends AbstractSystemTestUnit {
 		readByIds = gigaSpace.readByIds(IssuePojo.class, new Object[] { 100 });
 		resultsArray = readByIds.getResultsArray();
 		Assert.assertNull("Bad issue read", resultsArray[0]);
-	}
+
+        // read using sql query with like and rlike
+        SQLQuery<IssuePojo> query1 = new SQLQuery<IssuePojo>(IssuePojo.class.getName(), " reporter.username rlike 'da*k' ");
+        IssuePojo[] likeResult = gigaSpace.readMultiple(query1);
+        SQLQuery<IssuePojo> query2 = new SQLQuery<IssuePojo>(IssuePojo.class.getName(), " reporter.username like 'da%' ");
+        IssuePojo[] rlikeResult = gigaSpace.readMultiple(query2);
+        AssertUtils.assertEquivalent("", issuePojos,
+                Arrays.asList(likeResult));
+        AssertUtils.assertEquivalent("", issuePojos,
+                Arrays.asList(rlikeResult));
+
+    }
 
 	@Override
 	protected String getPUJar() {
