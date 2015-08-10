e0fab7154cff7534650a8b6aee4591f6cd5e2097
tamirt <tamirt@gigaspaces,com>
2015-08-10 15:10:43 +0300

    moved source from parent to itests + added plugin for json + exclude MongoSystemTestSuite so we can run each test separately via maven command

---
+++ itests/src/test/java/com/gigaspaces/persistency/qa/stest/BasicMongoSpaceAllInCacheTest.java
@@ -0,0 +1,43 @@
+package com.gigaspaces.persistency.qa.stest;
+
+import com.gigaspaces.persistency.qa.model.IssuePojo;
+import com.gigaspaces.persistency.qa.utils.AssertUtils;
+import junit.framework.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class BasicMongoSpaceAllInCacheTest extends AbstractSystemTestUnit {
+
+	@Override
+	@Test
+	public void test() {
+		List<IssuePojo> issuePojos = new ArrayList<IssuePojo>();
+
+		for (int i = 0; i < 15; i++) {
+			issuePojos.add(new IssuePojo(i + 1, "dank" + (i + 1)));
+		}
+
+		gigaSpace.writeMultiple(issuePojos.toArray(new IssuePojo[] {}));
+
+		waitForEmptyReplicationBacklog(gigaSpace);
+
+		List<IssuePojo> pojos = Arrays.asList(gigaSpace.readMultiple(
+				new IssuePojo(), 20));
+
+		Collections.sort(pojos);
+
+		Assert.assertEquals("size is not equals", issuePojos.size(),
+				pojos.size());
+
+		AssertUtils.assertEquivalent("", issuePojos, pojos);
+	}
+
+	@Override
+	protected String getPUJar() {
+		return "/all-in-cache-0.0.1-SNAPSHOT.jar";
+	}
+}
---
+++ itests/src/test/java/com/gigaspaces/persistency/qa/stest/MongoMirrorFailoverTest.java
@@ -0,0 +1,85 @@
+package com.gigaspaces.persistency.qa.stest;
+
+import com.gigaspaces.persistency.qa.model.IssueDocument;
+import com.gigaspaces.persistency.qa.utils.AssertUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * User: boris
+ * Date: 25/12/13
+ */
+public class MongoMirrorFailoverTest extends AbstractSystemTestUnit {
+
+    List<IssueDocument> issueDocs = new ArrayList<IssueDocument>();
+
+    @Override
+    @Test
+    public void test() {
+
+        gigaSpace.getTypeManager().registerTypeDescriptor(IssueDocument.getTypeDescriptor());
+        say("Mongo Mirror Failover test started ...");
+        Thread t = new Thread() {
+            public void run() {
+                try {
+                    //sleep in order to let mirror get restarted first
+                    Thread.sleep(1000);
+                    for (int i = 0; i < 10; i++) {
+                        IssueDocument doc = new IssueDocument ();
+                        doc.setKey(i+1);
+                        doc.setVotes(i+1);
+                        issueDocs.add(doc);
+                    }
+                    gigaSpace.writeMultiple(issueDocs.toArray(new IssueDocument[] {}));
+                } catch (InterruptedException e) {
+                    Assert.fail("fillSpace wait interrupted!");
+                }
+            }
+        };
+        t.start();
+        // shut down mirror when filling space
+        say("Failover mirror ... restarting");
+        restartPuGscs(mirrorServicePU,false);
+        //waiting in case the mirror and the test pu were in the same container
+        testPU.waitFor(4);
+        // wait till thread finished writing to space
+        try {
+            t.join();
+        } catch (InterruptedException e) {}
+
+        waitForEmptyReplicationBacklog(gigaSpace);
+
+        List<IssueDocument> docs = Arrays.asList(gigaSpace.readMultiple(
+                new IssueDocument(), 20));
+
+        Collections.sort(docs);
+
+        junit.framework.Assert.assertEquals("size is not equals", issueDocs.size(),
+                docs.size());
+
+        AssertUtils.assertEquivalent("", issueDocs, docs);
+
+        assertMongoEqualsSpace(issueDocs);
+
+        say("Mongo Mirror Failover test passed!");
+    }
+
+    @Override
+    protected String getPUJar() {
+        return "/lru-0.0.1-SNAPSHOT.jar";
+    }
+
+    private void assertMongoEqualsSpace(List<IssueDocument> beforeClear) {
+        clearMemory(gigaSpace);
+        junit.framework.Assert.assertEquals(0, gigaSpace.count(null));
+        List<IssueDocument> docs = Arrays.asList(gigaSpace.readMultiple(
+                new IssueDocument(), 20));
+        AssertUtils.assertEquivalent("space is not equivalent to mongo", beforeClear, docs);
+
+    }
+}
